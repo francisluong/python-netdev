bb77891d5c3dfdc469bcd15ab24147cea2195492
Francis Luong (Franco) <franco@definefunk.com>
2014-06-30 10:57:17 -0400

    added travis and nose

--- auth/userpass.py
+++ auth/userpass.py
@@ -13,25 +13,34 @@ class Userpass:
         self.iv = Random.new().read(16)
 
     def add_user_passwd (self, user, passwd):
+        """add a username/password"""
+        #set current_user if not set
+        if self.current_user == "":
+            self.current_user = user
         #track length of password per user
         self.length[user] = len(passwd)
         c = AES.new(self.key, self.mode, self.iv)
         self.database[user] = c.encrypt(passwd.ljust(self.bs))
 
     def user (self):
+        """return current user"""
         return self.current_user
 
     def passwd (self):
+        """return password for current user"""
         user = self.current_user
         length = self.length[user]
         c = AES.new(self.key, self.mode, self.iv)
         return c.decrypt(self.database[user])[0:length]
 
     def change_user(self, user):
+        """switch current user"""
         if user in self.database:
+            #only switch if user is already in the database
             self.current_user = user
 
     def load (self, filename):
+        """read in yaml file for password database"""
         import yaml
         import sys
         if sys.platform == "linux2":
@@ -42,16 +51,15 @@ class Userpass:
         users = yamldict["users"]
         firstuser = ""
         for user in users:
-            if firstuser == "":
-                firstuser = user
             self.add_user_passwd(user,users[user])
         if yamldict.has_key("defaultuser"):
             #set default user from defaultuser key
-            firstuser = yamldict["defaultuser"]
-        else:
-            #keep existing first user
-            firstuser = firstuser
-        self.change_user(firstuser)
+            self.change_user( yamldict["defaultuser"] )
 
     def keys(self):
+        """get list of users"""
         return self.database.keys()
+
+    def has_key(self, key):
+        """return True of user exists, else return False"""
+        return self.database.has_key(key)
